353cf5112b08b44708f135ee16bfb37c5c689de3
Aditya <adityaa0544@gmail.com>
2023-10-22 16:29:13 +0530

    hello

---
+++ src/LandingPage.jsx
@@ -0,0 +1,16 @@
+function LandingPage() {
+  return (
+    <div className="LandingGrid grid-row-7 bg-black">
+        <div className="LandingItem flex-1 justify-center align-middle">1</div>
+        <div className="LandingItem flex-1 justify-center align-middle">2</div>
+        <div className="LandingItem flex-1 justify-center align-middle">3</div>
+        <div className="LandingItem flex-1 justify-center align-middle">4</div>
+        <div className="LandingItem flex-1 justify-center align-middle">5</div>
+        <div className="LandingItem flex-1 justify-center align-middle">6</div>
+        <div className="LandingItem flex-1 justify-center align-middle">7</div>
+        
+    </div>
+  );
+}
+
+export default LandingPage;
